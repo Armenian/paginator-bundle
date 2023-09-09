a24a82c63975d38c6421f4310c924079b71eff79
David Akopyan <david@akopyan.lv>
2023-09-10 01:07:52 +0300

    - hf

--- src/DependencyInjection/PaginatorExtension.php
+++ src/DependencyInjection/PaginatorExtension.php
@@ -8,10 +8,9 @@ use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class PaginatorExtension extends Extension implements PrependExtensionInterface
+final class PaginatorExtension extends Extension
 {
     /**
      * @throws Exception
@@ -20,15 +19,6 @@ final class PaginatorExtension extends Extension implements PrependExtensionInte
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function prepend(ContainerBuilder $container): void
-    {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('prepend.yaml');
     }
 
     public function getAlias(): string
